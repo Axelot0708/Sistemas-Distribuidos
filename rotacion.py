b2d84a7854f464283057a439a13f9a195a205b71
Axelot0708 <axelaguilar0708@gmail.com>
2025-09-03 19:30:20 -0700

    Update rotacion.py

--- rotacion.py
+++ rotacion.py
@@ -3,56 +3,83 @@ import numpy as np
 
 def rot_x(x, y, z, theta):
     '''
-    Descripcion
+    Rotar un vector en el eje X
 
     Parameters:
-        param_name (data_type): brief description
-        param_name (data_type): brief description
+        x(float): valor de la coordenada X
+        y(float): valor de la coordenada Y
+        z(float): valor de la coordenada Z
+        theta(float): el angulo de rotación del vector
 
     Returns:
-        data_type: brief description
+        np.array([x, y, z]): devuelve el vector rotado en el angulo de theta en el eje X
     '''
-    pass
+    R = np.array([
+        [1, 0, 0],
+        [0, np.cos(theta), -np.sin(theta)],
+        [0, np.sin(theta),  np.cos(theta)]
+    ])
+    return R @ np.array([x, y, z])
 
 def rot_y(x, y, z, theta):
     '''
-    Descripcion
+    Rotar un vector en el eje Y
 
     Parameters:
-        param_name (data_type): brief description
-        param_name (data_type): brief description
+        x(float): valor de la coordenada X
+        y(float): valor de la coordenada Y
+        z(float): valor de la coordenada Z
+        theta(float): el angulo de rotación del vector
 
     Returns:
-        data_type: brief description
+        np.array([x, y, z]): devuelve el vector rotado en el angulo de theta en el eje Y
     '''
-    pass
+    R = np.array([
+        [ np.cos(theta), 0, np.sin(theta)],
+        [0, 1, 0],
+        [-np.sin(theta), 0, np.cos(theta)]
+    ])
+    return R @ np.array([x, y, z])
 
 def rot_z(x, y, z, theta):
     '''
-    Descripcion
+    Rotar un vector en el eje Z
 
     Parameters:
-        param_name (data_type): brief description
-        param_name (data_type): brief description
+        x(float): valor de la coordenada X
+        y(float): valor de la coordenada Y
+        z(float): valor de la coordenada Z
+        theta(float): el angulo de rotación del vector
 
     Returns:
-        data_type: brief description
+        np.array([x, y, z]): devuelve el vector rotado en el angulo de theta en el eje Z
     '''
-    pass
+    R = np.array([
+        [np.cos(theta), -np.sin(theta), 0],
+        [np.sin(theta),  np.cos(theta), 0],
+        [0, 0, 1]
+    ])
+    return R @ np.array([x, y, z])
 
 
 def rotar(x, y, z, theta, axis):
     '''
-    Descripcion
+    Rota un vector en un eje especifico
 
     Parameters:
-        param_name (data_type): brief description
-        param_name (data_type): brief description
-        param_name (data_type): brief description
-        param_name (data_type): brief description
-        axis (string): eje de rotacion; puede tomar valor 'x', 'y' o 'z'.
+        x (float): valor de la coordenada X
+        y (float): valor de la coordenada Y
+        z (float): valor de la coordenada Z
+        theta(float): el angulo de rotación del vector
+        axis(str): especifica el eje que se quiere rotar y se convierte a minúscula para que concuerden
 
     Returns:
-        data_type: brief description
+        np.array([x, y, z]): devuelve el vector rotado en el eje especificado en el angulo theta
     '''
-    pass
+    axis = axis.lower()
+    if axis == 'x':
+        return rot_x(x, y, z, theta)
+    elif axis == 'y':
+        return rot_y(x, y, z, theta)
+    elif axis == 'z':
+        return rot_z(x, y, z, theta)
